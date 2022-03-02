7f07beee13a40db7ea0878ad9854b89e9e76bcf7
mayank8887 <mayankdhyani.mbd@gmail.com>
2022-03-02 23:25:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const subtract=require("./subtract");
 
 
 console.log("sum:",add(2,4));
-console.log("reminder:",divide(9,3));
+console.log("Quotient:",divide(9,3));
 console.log("muliplication:",multiply(4,4));
 console.log("Subtraction:",subtract(5,3));
 
